809db9189d6014cdd26d54645dd9efa8aada4e2e
HEKUCHAN <heitorhirose@gmail.com>
2024-01-05 21:43:39 +0900

    Add join page

--- web/pages/join/[id].tsx
+++ web/pages/join/[id].tsx
@@ -1,9 +1,24 @@
 import { useRouter } from 'next/router';
+import VideoButton from '@/components/Button/VideoButton';
+import MicrophoneButton from '@/components/Button/MicrophoneButton';
+import SpeakerSelector from '@/components/Selecter/SpeakerSelector';
+
 
 const JoinRoom = () => {
   const router = useRouter();
   return (
-    <h1>{router.query.id}</h1>
+    <div className='flex flex-col'>
+      <div>
+        <h1>{router.query.id}</h1>
+      </div>
+      <div className='flex'>
+        <MicrophoneButton />
+        <VideoButton />
+      </div>
+      <div className='flex'>
+        <SpeakerSelector />
+      </div>
+    </div>
   )
 }
 
