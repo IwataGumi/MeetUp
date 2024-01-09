ab954c6e4dc222963079c74e2aa41bde30ea66fb
HEKUCHAN <heitorhirose@gmail.com>
2024-01-09 13:43:33 +0900

    Add button actions of main page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,22 +1,37 @@
-import Link from 'next/link';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] })
+import { useRouter } from 'next/router';
+import { useRef, useState } from 'react';
 
 const Home = () => {
+  const router = useRouter()
+  const [roomId, setRoomId] = useState('')
+
+  const joinRoom = () => {
+    router.push(`/join/${roomId}`)
+  }
+
   return (
-    <div className='hero'>
-      <div className="hero-content text-center w-full max-w-xs">
+    <div className='hero h-full min-h-screen'>
+      <div className="hero-content text-center m-auto w-full max-w-xs">
         <div className="flex flex-col items-center">
           <h1 className="text-5xl font-bold">MeetUp</h1>
           <p className="py-6">無料で使えるビデオ通話アプリ</p>
           <div className="join">
-            <input className="input join-item" placeholder="Room Code"/>
-            <button className="btn btn-neutral join-item">参加</button>
+            <input
+              value={roomId}
+              onChange={(e) => {setRoomId(e.target.value)}}
+              className="input input-bordered join-item"
+              placeholder="Room Code"
+            />
+            <button
+              onClick={joinRoom}
+              className="btn btn-neutral join-item"
+            >
+              参加
+            </button>
           </div>
           <div className="dropdown w-full">
-            <button tabIndex={0} className="btn w-full mt-2">ルーム作成</button>
-            <ul tabIndex={0} className="dropdown-content z-[1] menu mt-1 p-2 shadow bg-base-100 rounded-box w-52">
+            <button tabIndex={0} className="btn btn-neutral w-full mt-2">ルーム作成</button>
+            <ul tabIndex={0} className="dropdown-content z-[1] menu mt-1 p-2 shadow rounded-box w-52">
               <li><button>今すぐ作成をする</button></li>
               <li><button>予約する</button></li>
             </ul>
--- web/pages/join/[id].tsx
+++ web/pages/join/[id].tsx
@@ -1,25 +1,20 @@
 import { useRouter } from 'next/router';
+import SelfCamera from '@/components/Camera/SelfCamera';
 import VideoButton from '@/components/Button/VideoButton';
 import MicrophoneButton from '@/components/Button/MicrophoneButton';
 import SpeakerSelector from '@/components/Selecter/SpeakerSelector';
+import useDevices from '@/hooks/useDevices';
 
 
-const JoinRoom = () => {
-  const router = useRouter();
+const Join = () => {
+  const cameraDevices = useDevices('videoinput')
+  console.log(cameraDevices)
   return (
-    <div className='flex flex-col'>
-      <div>
-        <h1>{router.query.id}</h1>
-      </div>
-      <div className='flex'>
-        <MicrophoneButton />
-        <VideoButton />
-      </div>
-      <div className='flex'>
-        <SpeakerSelector />
-      </div>
+    <div className='flex flex-col justify-center items-center'>
+      <h1>Self Camera</h1>
+      <SelfCamera />
     </div>
   )
 }
 
-export default JoinRoom;
+export default Join;
