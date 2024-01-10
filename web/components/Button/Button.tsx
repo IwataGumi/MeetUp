3542e4e2c2207ae633c2f0c12f2594a93f1e913f
HEKUCHAN <heitorhirose@gmail.com>
2024-01-11 02:32:23 +0900

    Buttonの配置とカスタムクラスを追加した

---
+++ web/components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import { IconType } from "react-icons";
+
+interface Props {
+  className: string;
+  Icon: IconType;
+  onClick: () => void;
+}
+
+const BUTTON_CLASS = 'btn btn-circle w-[3.5rem] h-[3.5rem] min-h-[3.5rem]'
+
+const Button = ({
+  className, Icon, onClick
+}: Props) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${BUTTON_CLASS} ${className}`}
+    >
+      <Icon size={26} />
+    </button>
+  )
+}
+
+Button.defaultProps = {
+  className: ''
+}
+
+export default Button;
--- web/components/Button/ToggleButton.tsx
+++ web/components/Button/ToggleButton.tsx
@@ -2,27 +2,44 @@ import { IconType } from "react-icons";
 
 interface Props {
   state: boolean;
+  className: string;
+  ActiveClassName: string;
   ActiveIcon: IconType;
+  PassiveClassName: string;
   PassiveIcon: IconType;
   toggleFunction: () => void;
 }
 
+const BUTTON_CLASS = 'btn btn-circle w-[3.5rem] h-[3.5rem] min-h-[3.5rem]'
+
 const ToggleButton = ({
-  state, ActiveIcon, PassiveIcon, toggleFunction
+  state, className, ActiveClassName, ActiveIcon, PassiveClassName, PassiveIcon, toggleFunction
 }: Props) => {
   if (state) {
     return (
-      <button onClick={toggleFunction} className="btn btn-outline btn-circle">
-        <ActiveIcon size={18} />
+      <button
+        onClick={toggleFunction}
+        className={`${BUTTON_CLASS} ${className} ${ActiveClassName}`}
+      >
+        <ActiveIcon size={26} />
       </button>
     )
   } else {
     return (
-      <button onClick={toggleFunction} className="btn btn-circle btn-error">
-        <PassiveIcon size={18} />
+      <button
+        onClick={toggleFunction}
+        className={`${BUTTON_CLASS} ${className} ${PassiveClassName}`}
+      >
+        <PassiveIcon size={26} />
       </button>
     )
   }
+}
+
+ToggleButton.defaultProps = {
+  className: '',
+  ActiveClassName: '',
+  PassiceClassName: '',
 }
 
 export default ToggleButton;
