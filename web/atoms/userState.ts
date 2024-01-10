15b5157045f136c25878977bcdab384c6610e9d2
HEKUCHAN <heitorhirose@gmail.com>
2024-01-11 03:55:37 +0900

    Add User's information reicoil state

---
+++ web/atoms/userState.ts
@@ -0,0 +1,36 @@
+import { atom } from 'recoil';
+
+const LOCAL_STORAGE_NAME = 'userInfo';
+
+type UserType = {
+  username?: string;
+}
+
+const getUserInfo = () => {
+  if (typeof window === 'undefined') {
+    return {} as UserType;
+  }
+
+  const user = window.localStorage.getItem(LOCAL_STORAGE_NAME);
+  return user ? JSON.parse(user) as UserType : {} as UserType;
+}
+
+const saveItemToLocalStorage = (value: UserType) => {
+  if (typeof window === 'undefined') {
+    return;
+  }
+
+  window.localStorage.setItem(LOCAL_STORAGE_NAME, JSON.stringify(value));
+}
+
+export const userState = atom({
+  key: 'userState',
+  default: getUserInfo(),
+  effects: [
+    ({ onSet }) => {
+      onSet((newValue) => {
+        saveItemToLocalStorage(newValue);
+      })
+    }
+  ]
+})
