101b89c3a9f6aaae5c5105603113df8efe21b2e5
HEKUCHAN <heitorhirose@gmail.com>
2024-02-02 23:23:49 +0900

    #24 Change endpoint of websocket

--- api/api/web/ws/rooms/views.py
+++ api/api/web/ws/rooms/views.py
@@ -9,69 +9,18 @@ from pydantic import BaseModel, ValidationError
 
 logger = logger.bind(task="ws_Rooms")
 
-router = APIRouter
-
-class Command(BaseModel):
-    type: Literal["change_name", "webrtc_sdp", "webrtc_ice"]
-
-class JoinCommand(Command):
-    type: Literal["change_name"]
-    username: str
-
-class WebRTCSdpCommand(Command):
-    type: Literal["webrtc_sdp"]
-    description: str
-    src: str
-
-class WebRTCIceCommand(Command):
-    type: Literal["webrtc_ice"]
-    candidate: str
-    str: str
+router = APIRouter()  # Correct router initialization
 
 
-@router.websocket("/{room_uuid}")
+@router.websocket(path="/{room_uuid}")
 async def rooms_ws(
     websocket: WebSocket,
-    room_uuid: Annotated[uuid.UUID, Path(title="The uuid of room.")],
-    room_dao: RoomDAO = Depends(),
-    user: UserModel = Depends(with_authenticate),
 ):
-    room = await room_dao.get_room(room_uuid)
-
-    if room is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Not found Room."
-        )
-    room = await room_dao.get_users(room)
-
-    await room_dao.add_user(room, user)
-
     await websocket.accept()
-
     try:
         while True:
-            data = await websocket.receive_json()
-
-            try:
-                command = Command.model_validate_json(data)
-
-                match command.type:
-                    case 'change_name':
-                        break
-                    case 'webrtc_ice':
-                        break
-                    case 'webrtc_sdp':
-                        break
-            except ValidationError as e:
-                logger.warning(f"Unknown command type received: {data}")
+            data = await websocket.receive_text()
+            print(data)
+            await websocket.send_text(f"Message text was: {data}")
     except WebSocketDisconnect:
-        await room_dao.leave_user(user)
-        print('接続が切断されました。')
-
-
-
-
-
-
-    
+        print("切断しました。")
--- api/api/web/ws/router.py
+++ api/api/web/ws/router.py
@@ -1,4 +1,6 @@
+from api.web.ws import rooms
 from fastapi.routing import APIRouter
 
 ws_router = APIRouter()
 
+ws_router.include_router(rooms.router, prefix="/rooms")
