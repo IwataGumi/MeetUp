7a560732e305870dc2220a6dbf2aaf812fffb750
HEKUCHAN <heitorhirose@gmail.com>
2024-02-02 23:24:55 +0900

    temporally ui

--- web/pages/room/[id].tsx
+++ web/pages/room/[id].tsx
@@ -1,12 +1,34 @@
 import VideoControlls from "@/components/Camera/Controlls/VideoControlls";
 import VideoContainer from "@/components/Camera/VideoContainer";
+import { WebSocketURL } from "@/utils/static";
+import { useRouter } from "next/router";
+import React, { useCallback, useEffect, useState } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 
 const Room = () => {
+  const router = useRouter();
+  const [webSocktURL, setWebSocketURL] = useState(`${WebSocketURL}/ws/rooms/test`);
+
+  const { sendMessage } = useWebSocket(webSocktURL);
+
+  useEffect(() => {
+    if (router.query.id === undefined) return
+    setWebSocketURL(`${WebSocketURL}/ws/rooms/${router.query.id}`)
+    console.log(document.cookie)
+  }, [router.query])
+
+  const handleClickSendMessage = useCallback(() => sendMessage('Hello'), [sendMessage]);
+
   return (
     <>
       <div className="m-2 mt-16 flex flex-col">
+        {/*         
         <VideoContainer />
         <VideoControlls />
+         */}
+         <button className="btn btn-primary" onClick={handleClickSendMessage}>
+          Test
+         </button>
       </div>
     </>
   );
