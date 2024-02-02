e152a8233db5a779a9cc6544f328a97475c390c4
HEKUCHAN <heitorhirose@gmail.com>
2024-02-02 19:47:29 +0900

    #24 Create VideoContainer Component

--- web/pages/room/[id].tsx
+++ web/pages/room/[id].tsx
@@ -1,50 +1,11 @@
-import SelfCamera from "@/components/Camera/SelfCamera";
-import { useEffect, useRef, useState } from "react";
-import { videoMinimumSize } from '@/utils/static';
 import VideoControlls from "@/components/Camera/Controlls/VideoControlls";
+import VideoContainer from "@/components/Camera/VideoContainer";
 
 const Room = () => {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const users = [{}];
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  const [size, setSize] = useState<{ width: number; height: number }>({
-    width: 0,
-    height: 0,
-  });
-
-  const itemPerRow = Math.ceil(Math.sqrt((users?.length ?? 0)));
-  const row = Math.ceil(((users?.length ?? 0)) / itemPerRow);
-
-  useEffect(() => {
-    const element = wrapperRef.current;
-    if (!element || !users) return;
-    const observer = new ResizeObserver(() => {
-      let itemWidth = element.clientWidth / itemPerRow - 16;
-      let itemHeight = element.clientHeight / row - 16;
-
-      if (videoMinimumSize.width > itemWidth || videoMinimumSize.height > itemHeight) {
-        itemWidth = videoMinimumSize.width;
-        itemHeight = videoMinimumSize.height;
-      }
-
-      const scale = Math.min(itemWidth / 16, itemHeight / 9);
-      setSize({ width: Math.ceil(scale * 16), height: Math.ceil(scale * 9) });
-    });
-
-    observer.observe(element);
-    return () => {
-      observer?.disconnect();
-    };
-  }, [itemPerRow, row, users]);
-
   return (
     <>
       <div className="m-2 mt-16 flex flex-col">
-        <div className="h-[calc(100vh-64px-64px)] flex justify-center sm:items-center" ref={wrapperRef}>
-          <div style={{flex: '1 1 auto'}} className="h-full relative flex grow-[1 1 auto] flex-wrap justify-center">
-            <SelfCamera className="m-2 w-full h-full" width={size.width} height={size.height} />
-          </div>
-        </div>
+        <VideoContainer />
         <VideoControlls />
       </div>
     </>
