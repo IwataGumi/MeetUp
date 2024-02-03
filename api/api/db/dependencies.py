a3c6778c3772f87d3e10cb2006c0be566d9112d6
HEKUCHAN <heitorhirose@gmail.com>
2024-02-04 08:32:55 +0900

    #30 新しいデータベースの構造に変更する

--- api/api/db/dependencies.py
+++ api/api/db/dependencies.py
@@ -1,17 +1,17 @@
 from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.requests import Request
+from starlette.requests import HTTPConnection
 
 
-async def get_db_session(request: Request) -> AsyncGenerator[AsyncSession, None]:
+async def get_db_session(connection: HTTPConnection) -> AsyncGenerator[AsyncSession, None]:
     """
     Create and get database session.
 
     :param request: current request.
     :yield: database session.
     """
-    session: AsyncSession = request.app.state.db_session_factory()
+    session: AsyncSession = connection.app.state.db_session_factory()
 
     try:  # noqa: WPS501
         yield session
